b9a5a54fc970be30158356b1ae7cfdd63c093de8
markafarrell <mark.andrew.farrell@gmail.com>
2016-11-01 11:53:00 +1100

    2Hz Output

--- attiny2313/blinky.c
+++ attiny2313/blinky.c
@@ -1,6 +1,7 @@
 // this is the header file that tells the compiler what pins and ports, etc.
 // are available on this chip.
 #include <avr/io.h>
+#include <util/delay.h>
 
 // define what pins the LEDs are connected to.
 // in reality, PD6 is really just '6'
@@ -12,30 +13,19 @@
 #define set_input(portdir,pin) portdir &= ~(1<<pin)
 #define set_output(portdir,pin) portdir |= (1<<pin)
 
-// this is just a program that 'kills time' in a calibrated method
-void delay_ms(uint16_t ms) {
-  uint16_t delay_count = F_CPU / 72000;
-  volatile uint16_t i;
-
-  while (ms != 0) {
-    for (i=0; i != delay_count; i++);
-    ms--;
-  }
-}
-
 int main(void) {
   // initialize the direction of PORTD #6 to be an output
   set_output(DDRA, LED);  
 
   while (1) {
-	uint16_t time = 200;
+	uint16_t time = 1000;
   
-    // turn on the LED for 200ms
+    // turn on the LED for 1000ms
     output_high(PORTA, LED);
-    delay_ms(time);
-    // now turn off the LED for another 200ms
+    _delay_ms(time);
+    // now turn off the LED for another 1000ms
     output_low(PORTA, LED);
-    delay_ms(time);
+    _delay_ms(time);
     // now start over
   }
 }
